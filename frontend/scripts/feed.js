75a1c08204336145b381d433cbad759575473b1e
Khodor Hassoun <khodorhassoun@outlook.com>
2022-10-03 14:53:19 +0300

    Feed.html: Include js file

---
+++ frontend/scripts/feed.js
@@ -0,0 +1,7 @@
+// Navbar buttons
+const userProfileButton = document.getElementById('user-profile-pic')
+const userChatButton = document.getElementById('user-chat-pic');
+const userLogoutButton = document.getElementById('user-logout-pic')
+
+// feed container 
+const feedContainer = document.querySelector('.feed-container')
